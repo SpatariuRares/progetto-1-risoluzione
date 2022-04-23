7724cbeeefd19106ddd0d173b33bb2ecb9d19d0c
Rares Spatariu <sararares12@gmail.com>
2022-04-24 00:08:18 +0200

    first commit

---
+++ src/List.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+const List = (props) => {
+  return <ul className="user-list">
+    {props.data.map((person) => (
+      <li key={person.id}>
+        <Person {...person} removeItem={props.removeItem} />
+      </li>
+    ))}
+  </ul>;
+};
+
+const Person = ({ id, nome, stato, img, removeItem }) => {
+  console.log(id)
+  return <article>
+    <img src={img} alt="prs" className="person-img" />
+    <div className="person-info">
+        <div className="person-action">
+          <h4>{nome}</h4>
+          <button className="btn" onClick={() => removeItem(id)}>
+            {" "}
+            X{" "}
+          </button>
+        </div>
+        <p>{stato}</p>
+      </div>
+  </article>;
+};
+
+export default List;
